db1454cc51a31695f8361537e5abf6e37f2989d9
Administrator <Administrator@USER-20160408CZ>
2016-04-13 22:54:41 +0800

    init slf-log4j

--- src/main/java/com/math040/gambling/controller/DebtController.java
+++ src/main/java/com/math040/gambling/controller/DebtController.java
@@ -2,6 +2,8 @@ package com.math040.gambling.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,16 @@ import com.math040.gambling.service.DebtService;
 @Controller
 @RequestMapping("/debt")
 public class DebtController {
+	
+	private static Logger logger = LoggerFactory.getLogger(DebtController.class);
+	
 	@Autowired
 	private DebtService debtService;
 	
 	@RequestMapping(value="/list", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Debt> list(){
+		logger.error("DebtController.list");
 		return debtService.findAll();
 	}
 	
