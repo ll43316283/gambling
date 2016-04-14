8c1521c95b3620131b68af93a40caed9bbf5c119
Administrator <Administrator@USER-20160408CZ>
2016-04-14 21:56:20 +0800

    use hibernate entitymanager to replace commons-loggings to prevent the confliction of slf4j implements

--- src/main/java/com/math040/gambling/config/JpaConfig.java
+++ src/main/java/com/math040/gambling/config/JpaConfig.java
@@ -8,7 +8,7 @@ import javax.sql.DataSource;
 import org.h2.jdbcx.JdbcConnectionPool;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories; 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
